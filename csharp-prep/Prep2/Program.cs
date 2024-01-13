5d4de282e0d1a0d289211b9fdc56ab72f3165050
lodakins <loadkins99@gmail.com>
2024-01-13 20:43:13 +0100

    finshed working on Prep 1 to 3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        
+        Console.WriteLine("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        // if(grade >= 90){
+        //     Console.WriteLine("A");
+        // }else if(grade >= 80){
+        //     Console.WriteLine("B");
+        // }else if(grade >= 70){
+        //     Console.WriteLine("C");
+        // }else if(grade >= 60){
+        //     Console.WriteLine("D");
+        // }else{
+        //     Console.WriteLine("F");
+        // }
+       
+        string letter="";
+
+        if(grade >= 90){
+            letter= "A";
+        }else if(grade >= 80){
+            letter= "B";
+        }else if(grade >= 70){
+            letter= "C";
+        }else if(grade >= 60){
+            letter= "D";
+        }else{
+            letter= "F";
+        }
+
+        Console.WriteLine(letter);
+
+         if(grade >= 70){
+            Console.WriteLine("Congratulations!!!, you passed the course.");
+        }else{
+            Console.WriteLine("You failed the course. Try Again.");
+        }
+
+
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomgenerator = new Random();
+
+        
+
+        bool correct = false;
+        int noOfGuess=0;
+        bool retry = false;
+
+        // Console.WriteLine("What is your magic number? ");
+        // int magicnumber = int.Parse(Console.ReadLine());
+
+        int magicnumber = randomgenerator.Next(1,101);
+        Console.WriteLine(magicnumber);
+        
+
+        do{
+            Console.WriteLine("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            noOfGuess++;
+            if(guess > magicnumber){
+                Console.WriteLine("Higher");
+            }else if( guess < magicnumber){
+                Console.WriteLine("Lower");
+            }else{
+                Console.WriteLine($"Congratulations!!!. You guessed it. You guess {noOfGuess} times. ");
+                correct=true;
+                Console.WriteLine("Do you want to retry? Y or N");
+                string response = Console.ReadLine();
+                if(response =="Y"){
+                    retry= true;
+                    noOfGuess=0;
+                    magicnumber = randomgenerator.Next(1,101);
+                }
+            }
+
+        }while(!correct || !!retry);
+
+        
+
     }
 }
