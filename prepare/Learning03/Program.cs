5eba9f024c340dadbc093a1be92fa7d08dc14328
lodakins <loadkins99@gmail.com>
2024-01-28 23:04:00 +0100

    feat:encapsulation practice

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction  fraction  = new Fraction();
+        Fraction fraction_1 = new Fraction(10);
+        Fraction fraction_2 = new Fraction(10,2);
+
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+        
+        Console.WriteLine(fraction_1.GetFractionString());
+        Console.WriteLine(fraction_1.GetDecimalValue());
+
+        Console.WriteLine(fraction_2.GetFractionString());
+        Console.WriteLine(fraction_2.GetDecimalValue());
     }
+    
 }
